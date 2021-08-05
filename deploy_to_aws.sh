a8bbeb9bf4dfe1bb39e4bf34b87a16f59d8f65fa
Sujay khandekar <sujaykhandek@gmail.com>
2021-08-05 16:48:10 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -3,7 +3,7 @@ echo 'Starting to Deploy...'
 ssh ubuntu@3.234.99.37  "
         sudo su -l sujay
         cd /newstorage/users/sujay/R_check
-       
+        mkdir temp_sdk2
         echo 'directory created'
         git fetch origin
         echo 'fetching done'
