24a0d7ab9a324df5ccdb111e9ab74c0eb75111f4
Sujay khandekar <sujaykhandek@gmail.com>
2021-08-05 16:17:44 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
-ssh ubuntu@3.234.99.37  " 
+ssh ubuntu@3.234.99.37  "
+        sudo docker image prune -f
         cd /newstorage/users/sujay/R_check 
-        mkdir test_dir
+         
 "
 echo 'Deployment completed successfully'
