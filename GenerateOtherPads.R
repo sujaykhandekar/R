f6c85a66cda0db57ce04ac5cb78a6ad20963d573
Sujay khandekar <sujaykhandek@gmail.com>
2021-08-05 16:45:27 -0400

    Update GenerateOtherPads.R

--- GenerateOtherPads.R
+++ GenerateOtherPads.R
@@ -2,7 +2,7 @@
 # Author: Jitender Aswani, Co-Founder @datadolph.in
 # Date: 3/15/2013
 # Copyright (c) 2011, under the Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0) License
-# For more information see: https://creativecommons.org/licenses/by-nc/3.0/
+# For more infrmation see: https://creativecommons.org/licenses/by-nc/3.0/
 # All rights reserved.
 
 # PADS Related to Recessions, Social Networks, Olympic Medals, Failed Banks
