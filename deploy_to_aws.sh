72f906a281af00479972d7732f1bfc95ae1e580b
Sujay khandekar <sujaykhandek@gmail.com>
2021-08-05 17:38:17 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
 ssh ubuntu@3.234.99.37  "
-        sudo su -l sujay
-        cd /newstorage/users/sujay/R_check
+        #sudo su -l sujay
+        cd /home/ubuntu/temp_github_actions/R_check
         ##mkdir temp_sdk2
         echo 'directory created'
         git pull 
