c51f14dbf0f0aeeb9ca7c446b7117251962182c8
Sujay khandekar <sujaykhandek@gmail.com>
2021-08-05 16:23:12 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
 ssh ubuntu@3.234.99.37  "
-        
-        cd /newstorage/users/sujay/R_check
+        sudo su sujay "sujay"
+        cd R_check
         git fetch origin
         echo 'fetching done'
          
